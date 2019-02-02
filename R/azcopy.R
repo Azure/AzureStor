9ca33c1f5775568c4b655449351e1e92487c755a
hong-revo <hongooi@microsoft.com>
2019-02-03 03:25:35 +1100

    call azcopy implementation

--- R/azcopy.R
+++ R/azcopy.R
@@ -1,3 +1,31 @@
+#' Call the azcopy file transfer utility
+#'
+#' @param ... Arguments to pass to azcopy. If no arguments are supplied, azcopy will print a help screen.
+#' @param force For `azcopy_login`, whether to force azcopy to relogin.
+#'
+#' @rdname azcopy
+#' @export
+call_azcopy <- function(...)
+{
+    azcopy <- get_azcopy_path()
+    args <- sapply(list(...), as.character)
+    system2(azcopy, args)
+}
+
+
+#' @rdname azcopy
+#' @export
+azcopy_login <- function(force=FALSE)
+{
+    if(exists("azcopy_logged_in", envir=.AzureStor) && isTRUE(.AzureStor$azcopy_logged_in) && !force)
+        return(invisible(NULL))
+    res <- call_azcopy("login")
+    if(res == 0)
+        .AzureStor$azcopy_logged_in <- TRUE
+    invisible(NULL)
+}
+
+
 # azcopy unset/NULL -> not initialized
 # azcopy = NA -> binary not found, or version < 10 (not usable)
 # azcopy = path -> usable
@@ -36,24 +64,76 @@ set_azcopy_path <- function(path="azcopy")
     }
 
     .AzureStor$azcopy <- unname(path)
+    message("Using azcopy binary ", path)
     invisible(NULL)
 }
 
 
 azcopy_upload <- function(container, src, dest, ...)
 {
-    call_azcopy(...)
+    auth <- check_azcopy_auth(container)
+
+    if(attr(auth, "method") == "key")
+    {
+        acctname <- sub("\\..*$", "", httr::parse_url(container$endpoint$url)$host)
+        Sys.setenv(ACCOUNT_NAME=acctname, ACCOUNT_KEY=auth)
+        on.exit(Sys.unsetenv(c("ACCOUNT_NAME", "ACCOUNT_KEY")))
+    }
+    else if(attr(auth, "method") == "token")
+        azcopy_login()
+    else if(attr(auth, method) == "sas")
+        dest <- paste0(dest, "?", auth)
+
+    call_azcopy("copy", src, dest, ...)
 }
 
 
 azcopy_download <- function(container, src, dest, ...)
 {
-    call_azcopy(...)
+    auth <- check_azcopy_auth(container)
+
+    if(attr(auth, "method") == "key")
+    {
+        acctname <- sub("\\..*$", "", httr::parse_url(container$endpoint$url)$host)
+        Sys.setenv(ACCOUNT_NAME=acctname, ACCOUNT_KEY=auth)
+        on.exit(Sys.unsetenv(c("ACCOUNT_NAME", "ACCOUNT_KEY")))
+    }
+    else if(attr(auth, "method") == "token")
+        azcopy_login()
+    else if(attr(auth, method) == "sas")
+        src <- paste0(src, "?", auth)
+
+    call_azcopy("copy", src, dest, ...)
 }
 
 
-call_azcopy <- function(...)
+check_azcopy_auth <- function(container, key, token, sas)
 {
-    azcopy <- get_azcopy_path()
-    stop("Not yet implemented")
+    endpoint <- container$endpoint
+
+    if(inherits(container, "blob_container"))
+    {
+        if(!is.null(endpoint$token))
+            return(structure(0, method="token"))
+        if(!is.null(endpoint$sas))
+            return(structure(endpoint$sas, method="sas"))
+        warning("No supported authentication method found for blob; defaulting to public", call.=FALSE)
+        return(structure(0, method="public"))
+    }
+
+    if(inherits(container, "file_share") && !is.null(endpoint$sas))
+        return(structure(endpoint$sas, method="sas"))
+
+    if(inherits(container, "adls_filesystem"))
+    {
+        if(!is.null(endpoint$key))
+        {
+            warning("Authenticating with a shared key is discouraged")
+            return(structure(endpoint$key, method="key"))
+        }
+        if(!is.null(endpoint$token))
+            return(structure(0, method="token"))
+    }
+
+    stop("No supported authentication method", call.=FALSE)
 }
