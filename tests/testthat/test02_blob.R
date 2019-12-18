9e7e196cc6df529c8d0a63ae9b5516ced00b9ac1
Christian Grotheer <christian.grotheer@prognostica.de>
2019-12-18 14:07:37 +0100

    Added ability to check if a blob exists

--- tests/testthat/test02_blob.R
+++ tests/testthat/test02_blob.R
@@ -134,6 +134,10 @@ test_that("Blob client interface works",
     iris3 <- as.data.frame(jsonlite::fromJSON(con))
     expect_identical(iris, iris3)
 
+    # check blob
+    expect_false(check_blob(cont, "nonexisting.blob"))
+    expect_true(check_blob(cont, "iris.json"))
+
     # ways of deleting a container
     delete_blob_container(cont, confirm=FALSE)
     delete_blob_container(bl, "newcontainer2", confirm=FALSE)
